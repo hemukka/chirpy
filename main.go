f412decf86cc4918cdd1a841ddda82acddf2ea36
hemukka <henri.specops@hotmail.com>
2025-06-29 00:21:01 +0300

    limit endpoints to specific HTTP methods only and move them to /api namespace

--- main.go
+++ main.go
@@ -21,9 +21,9 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc("/healthz", handlerReadiness)
-	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("/reset", apiCfg.handlerMetricsReset)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /api/reset", apiCfg.handlerMetricsReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
